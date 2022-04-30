520a72f611aebf36ece2b68d5c90df593db719f1
FelixDes <vip.zatebe@mail.ru>
2022-05-01 01:15:42 +0300

    More struct elements added

--- main.py
+++ main.py
@@ -11,25 +11,32 @@ app.config['SECRET_KEY'] = \
 
 @app.route("/")
 def index():
-    return render_template("index.html", content=build_html_for_json("static/raw_templates/index.html", "static/text"
-                                                                                                        "/text.json",
-                                                                     "rus"))
+    return render_template(
+        "index.html", dct=get_dict_for_from_json(get_string_from_path("static/text/text.json"), "rus"))
 
 
-def build_html_for_json(html_path, json_path, lang) -> str:
-    dct = json.loads(get_string_from_path(json_path))
-    html = get_string_from_path(html_path)
-    match = re.search(r'{{\w+}}', html)
-    while match is not None:
-        key = html[match.start(): match.end()]
-        html = html.replace(key, dct[key.replace("{{", "").replace("}}", "")][lang]["text"].replace("\n", "<br>"))
-        match = re.search(r'{{\w+}}', html)
-    return html
+# def build_html_for_json(html_path, json_path, lang) -> str:
+#     regex = r'\|\|\w+\|\|'
+#
+#     dct = json.loads(get_string_from_path(json_path))
+#     html = get_string_from_path(html_path)
+#     match = re.search(regex, html)
+#     while match is not None:
+#         key = html[match.start(): match.end()]
+#         html =
+#           html.replace(key, str(dct[key.replace("||", "").replace("||", "")][lang]["data"].replace("\n", "<br>")))
+#         match = re.search(regex, html)
+#     return html
 
 
 def get_string_from_path(path):
     with open(path, "r") as file:
         return file.read()
+
+
+def get_dict_for_from_json(json_string, lang):
+    jsn = dict(json.loads(json_string))
+    return {key: jsn[key][lang] for key in jsn.keys()}
 
 
 if __name__ == '__main__':
