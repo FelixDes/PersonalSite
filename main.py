ebbccde5504950a8304aae83caa0fb4abb726fad
FelixDes <vip.zatebe@mail.ru>
2022-05-02 15:52:24 +0300

    Articles added

--- main.py
+++ main.py
@@ -9,38 +9,19 @@ app.config['SECRET_KEY'] = \
     "_th{zi{qxXpNwPPWBkmdRtw}akIfadgOjtrdygLmGhb{OBawz^GIU|qpFYiZcBAhb{r]ZNr]ZzgRzqhqeZBBpo;hv_oeBnXZz{'"
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        if request.form.get('change') == 'eng':
-            return render_template(
-                "index.html", dct=get_dict_for_from_json(get_string_from_path("static/text/text.json"), "eng"))
-        elif request.form.get('change') == 'rus':
-            return render_template(
-                "index.html", dct=get_dict_for_from_json(get_string_from_path("static/text/text.json"), "rus"))
+    # , methods=['GET', 'POST']
+    # if request.method == 'POST':
+    #     if request.form.get('change') == 'eng':
+    #         return render_template(
+    #             "index.html", dct=get_dict_for_from_json(get_string_from_path("static/text/text.json"), "eng"))
+    #     elif request.form.get('change') == 'rus':
+    #         return render_template(
+    #             "index.html", dct=get_dict_for_from_json(get_string_from_path("static/text/text.json"), "rus"))
 
     return render_template(
-        "index.html", dct=get_dict_for_from_json(get_string_from_path("static/text/text.json"), "eng"))
-
-
-# @app.route("/")
-# def index():
-#     return render_template(
-#         "index.html", dct=get_dict_for_from_json(get_string_from_path("static/text/text.json"), "rus"))
-#
-
-# def build_html_for_json(html_path, json_path, lang) -> str:
-#     regex = r'\|\|\w+\|\|'
-#
-#     dct = json.loads(get_string_from_path(json_path))
-#     html = get_string_from_path(html_path)
-#     match = re.search(regex, html)
-#     while match is not None:
-#         key = html[match.start(): match.end()]
-#         html =
-#           html.replace(key, str(dct[key.replace("||", "").replace("||", "")][lang]["data"].replace("\n", "<br>")))
-#         match = re.search(regex, html)
-#     return html
+        "index.html", dct=get_dict_for_from_json(get_string_from_path("static/text/text.json"), "rus"))
 
 
 def get_string_from_path(path):
@@ -52,6 +33,11 @@ def get_dict_for_from_json(json_string, lang):
     jsn = dict(json.loads(json_string))
     return {key: jsn[key][lang] for key in jsn.keys()}
 
+# def link_preprocessor(dct):
+#     for node in dct:
+#         if node["link_preprocessor"] is not None:
+#             if type(node["text"]) == list:
+#                 for
 
 if __name__ == '__main__':
     app.run(port=10000)
